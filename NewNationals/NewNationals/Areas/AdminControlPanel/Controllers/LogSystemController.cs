13792249f3ccdc557b51cf5288b65e2d37e20353
bacnd <nguyenducbachy@gmail.com>
2016-12-12 15:29:14 +0700

    Nhật ký hệ thống

---
+++ NewNationals/NewNationals/Areas/AdminControlPanel/Controllers/LogSystemController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary.Services;
+using NewNationals.Areas.AdminControlPanel.Models;
+using PagedList;
+
+namespace NewNationals.Areas.AdminControlPanel.Controllers
+{
+    public class LogSystemController : BaseController
+    {
+        // GET: AdminControlPanel/LogSystem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        LogSystemService logService = new LogSystemService();
+
+        public ActionResult ListLogSystem(int? page, string SearchString, string FromDate, string ToDate)
+        {
+            int pageNum = page ?? 1;
+            var showlist = logService.GetByListAllOrderDesc();
+            var listlog = new List<LogSystems>();
+            foreach (var item in showlist)
+            {
+                listlog.Add(new LogSystems()
+                {
+                    LogId = item.LogId,
+                    CreateDate = item.CreateDate,
+                    IPAddress = item.IPAddress,
+                    Messenger = item.Messenger,
+                    Status = item.Status
+                });
+            }
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                showlist = showlist.Where(x => x.Messenger.Contains(SearchString) || x.IPAddress.Contains(SearchString));
+            }
+            try
+            {
+                if (!string.IsNullOrEmpty(FromDate))
+                {
+                    if (!FromDate.Trim().Equals(string.Empty))
+                    {
+                        showlist = showlist.Where(x => x.CreateDate.Date >= DateTime.Parse(FromDate).Date);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            try
+            {
+                if (!string.IsNullOrEmpty(ToDate))
+                {
+                    if (!ToDate.Trim().Equals(string.Empty))
+                    {
+                        showlist = showlist.Where(x => x.CreateDate.Date <= DateTime.Parse(ToDate).Date);
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return View(showlist.ToPagedList(pageNum, 30));
+        }
+    }
+}
---
+++ NewNationals/NewNationals/Areas/AdminControlPanel/Models/LogSystems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewNationals.Areas.AdminControlPanel.Models
+{
+    public class LogSystems
+    {
+        [Key]
+        public long LogId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string IPAddress { get; set; }
+
+        public string Messenger { get; set; }
+
+        public bool? Status { get; set; }
+    }
+}
