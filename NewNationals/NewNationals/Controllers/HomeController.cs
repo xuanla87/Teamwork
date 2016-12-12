6df7cb646f020cf2b8f6a74a08e26814879d943a
ANHXUAN\xuanla87 <xuanla@niemtinvn.com>
2016-12-12 10:29:53 +0700

    NEWS CODE

---
+++ NewNationals/NewNationals/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewNationals.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Pages(string slug)
+        {
+            return PartialView();
+        }
+
+        public ActionResult Details(string slug)
+        {
+            return PartialView();
+        }
+
+        public ActionResult Categories(string slug, int? page)
+        {
+            return PartialView();
+        }
+
+        public ActionResult PageError()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Tags(string slug)
+        {
+            return PartialView();
+        }
+
+        public ActionResult Default(string slug, int? page)
+        {
+            if (slug == null)
+            {
+
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+    }
+}
