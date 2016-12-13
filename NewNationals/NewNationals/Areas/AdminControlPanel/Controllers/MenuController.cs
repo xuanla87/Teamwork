5a57aa1cce8024a9bd115e55c252fa5e1844a21a
bacnd <nguyenducbachy@gmail.com>
2016-12-13 21:58:28 +0700

    cập nhật admin : Menu

---
+++ NewNationals/NewNationals/Areas/AdminControlPanel/Controllers/MenuController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibrary.Commons;
+using ClassLibrary.Models;
+using ClassLibrary.Services;
+using NewNationals.Areas.AdminControlPanel.Models;
+using PagedList;
+
+namespace NewNationals.Areas.AdminControlPanel.Controllers
+{
+    public class MenuController : Controller
+    {
+        MenuService menuService=new MenuService();
+        // GET: AdminControlPanel/Menu
+        public ActionResult Index(int? page, string SearchString)
+        {
+            int pageNum = page ?? 1;
+            var showlist = menuService.ListAllMenu();
+            var listslide = new List<Menu>();
+            foreach (var item in showlist)
+            {
+                listslide.Add(new Menu()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    TargetUrl = item.TargetUrl,
+                    ParentId = item.ParentId,
+                    Tanoxomy = item.Tanoxomy,
+                    Order = item.Order,
+                    stExtension = item.stExtension
+                });
+            }
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                showlist = showlist.Where(x => x.Name.Contains(SearchString));
+            }
+            return View(showlist.ToPagedList(pageNum, 20));
+        }
+        #region [Insert]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.ParentId = new SelectList(menuService.GetMenuByParent(), "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MenuModels entity)
+        {
+            ViewBag.ParentId = new SelectList(menuService.GetMenuByParent(), "Id", "Name");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var mn = new Menu();
+                    mn.Id = 1;
+                    mn.Name = entity.Name;
+                    mn.TargetUrl = entity.TargetUrl;
+                    mn.ParentId = entity.ParentId;
+                    mn.Tanoxomy = entity.Tanoxomy;
+                    mn.Order = entity.Order;
+                    mn.stExtension = entity.stExtension;
+                    menuService.Insert(mn);
+                    return RedirectToAction("Index", "Menu");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Lỗi!");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Lỗi!");
+                return View();
+            }
+        }
+        #endregion
+        #region [Update]
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var entity = menuService.GetMenuById(int.Parse(id.ToString()));
+            if (entity == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParentId = new SelectList(menuService.GetMenuByParent(), "Id", "Name");
+            MenuModels mn = new MenuModels();
+            mn.Id = entity.Id;
+            mn.Name = entity.Name;
+            mn.TargetUrl = entity.TargetUrl;
+            mn.ParentId = entity.ParentId;
+            mn.Tanoxomy = entity.Tanoxomy;
+            mn.Order = entity.Order;
+            mn.stExtension = entity.stExtension;
+            return View(mn);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(MenuModels entity)
+        {
+            ViewBag.ParentId = new SelectList(menuService.GetMenuByParent(), "Id", "Name");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var mn = new Menu();
+                    mn.Id = entity.Id;
+                    mn.Name = entity.Name;
+                    mn.TargetUrl = entity.TargetUrl;
+                    mn.ParentId = entity.ParentId;
+                    mn.Tanoxomy = entity.Tanoxomy;
+                    mn.Order = entity.Order;
+                    mn.stExtension = entity.stExtension;
+                    menuService.Update(mn);
+                    return RedirectToAction("Index", "Menu");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Lỗi!");
+                    return View();
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Lỗi!");
+                return View();
+            }
+        }
+        #endregion
+        #region [Delete]
+        [HttpPost]
+        public JsonResult Delete(int id)
+        {
+            var data = true;
+            try
+            {
+                var menu = menuService.GetMenuById(id);
+                if (menu != null)
+                {
+                    // thay đổi trạng thái về -1 coi như bản ghi đã bị xóa
+                    menuService.Delete(id);
+                }
+            }
+            catch (Exception)
+            {
+                data = false;
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
+    }
+}
---
+++ NewNationals/NewNationals/Areas/AdminControlPanel/Models/MenuModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewNationals.Areas.AdminControlPanel.Models
+{
+    public class MenuModels
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [Display(Name = "Tên menu")]
+        public string Name { get; set; }
+
+        [StringLength(350)]
+        [Display(Name = "Đường dẫn bài viết")]
+        public string TargetUrl { get; set; }
+
+        [Display(Name = "Menu cha")]
+        public long? ParentId { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Phân loại")]
+        public string Tanoxomy { get; set; }
+
+        [Display(Name = "Vị trí sắp xếp")]
+        public byte Order { get; set; }
+
+        [StringLength(150)]
+        [Display(Name = "Mở rộng")]
+        public string stExtension { get; set; }
+    }
+}
