f974bc4358728447ad6801546c07ddce92871da0
ANHXUAN\xuanla87 <xuanla@niemtinvn.com>
2016-12-15 15:54:58 +0700

    ADD code

---
+++ NewNationals/NewNationals/Scripts/menu.js
@@ -0,0 +1,8 @@
+﻿$('.nav li:has(ul)').addClass('menu-item-has-children');
+$('.nav li a[href="' + location.pathname + '"]').parent('li').addClass('current-menu-item');
+$('.nav li:has(li.current-menu-item)').addClass('current-menu-parent');
+
+$('.category li:has(ul)').addClass('menu-item-has-children');
+$('.category li a[href="' + location.pathname + '"]').parent('li').addClass('current-menu-item');
+$('.category li:has(li.current-menu-item)').addClass('current-menu-parent');
+$('.category li a[href="' + location.pathname + '"]').addClass('current-menu-item');
