90f00b7bc560f3a055900e4201048a54e97b78e7
bacnd <nguyenducbachy@gmail.com>
2016-12-16 16:01:16 +0700

    cập nhật editor

--- NewNationals/NewNationals/Areas/AdminControlPanel/Content/ckfinder/config.js
+++ NewNationals/NewNationals/Areas/AdminControlPanel/Content/ckfinder/config.js
@@ -5,12 +5,8 @@ For licensing, see LICENSE.html or http://cksource.com/ckfinder/license
 
 CKFinder.customConfig = function( config )
 {
-	// Define changes to default configuration here.
-	// For the list of available options, check:
-	// http://docs.cksource.com/ckfinder_2.x_api/symbols/CKFinder.config.html
-
-	// Sample configuration options:
-	// config.uiColor = '#BDE31E';
-	// config.language = 'fr';
-	// config.removePlugins = 'basket';
+    config.language = 'vi';
+    config.uiColor = '#F7B42C';
+    config.height = 300;
+    config.toolbarCanCollapse = true;
 };
